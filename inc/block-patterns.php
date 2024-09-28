100dfad40ffbd37dfa2ea8a5dcc9b0355ae2ffbb
Piotr Kostrzewski <piotr@192.168.1.2>
2024-09-28 16:49:54 +0200

    Files update

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -1,18 +1,3 @@
 <?php
-function recruitment_task_register_patterns_and_categories() {
-    // Rejestracja kategorii wzorców
-    register_block_pattern_category('section-patterns', array('label' => __('Sections', 'recruitment-task')));
-
-    // Rejestracja wzorca bloków
-    register_block_pattern(
-        'recruitment-task/latest-posts-pattern',
-        array(
-            'title'       => __( 'Latest Posts Pattern', 'recruitment-task' ),
-            'description' => _x( 'A section that shows the latest posts.', 'Block pattern description', 'recruitment-task' ),
-            'content'     => '<!-- wp:recruitment-task/latest-posts /-->',
-            'categories'  => array('section-patterns'),
-            'keywords'    => array( 'latest', 'posts' )
-        )
-    );
-}
-add_action('init', 'recruitment_task_register_patterns_and_categories');
+//latest-posts pattern
+require_once __DIR__ . '/blocks/latest-posts/pattern-registration.php';
