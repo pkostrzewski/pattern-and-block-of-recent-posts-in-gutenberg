48dd262c4781bc333238bb4266579c6dde82b249
Piotr Kostrzewski <piotr@192.168.1.2>
2024-09-28 16:54:56 +0200

    File update

--- build/BlockLatestPosts.asset.php
+++ build/BlockLatestPosts.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '8cec799f62dcd84fd0a9');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '5a9ee43bf41d4d3ed8ea');
